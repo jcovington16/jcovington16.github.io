150e6960c6a456236bde6e0dfd7a00d16dc32934
jcovington16 <covingtonjoshua7@gmail.com>
2021-09-20 20:56:51 -0600

    added the main section of the page with keyframe animation. working on adding place holders for the projects portion. will soon work on responsiveness afterwards.

---
+++ index.js
@@ -0,0 +1,26 @@
+const words = ['Developer', 'Engineer', 'Machine', 'Creator'];
+
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+
+(function type() {
+
+    if(count === words.length){
+        count = 0;
+    }
+
+    currentText = words[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400)
+
+}());
